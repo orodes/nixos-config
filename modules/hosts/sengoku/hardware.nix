1bd335728b657b1db6463f0c445141826557f231
orodes <198293654+orodes@users.noreply.github.com>
2026-05-23 21:04:52 +1000

    feat/fix: nvme disk fix

--- modules/hosts/sengoku/hardware.nix
+++ modules/hosts/sengoku/hardware.nix
@@ -5,6 +5,8 @@
     {
       imports = [ ./_hardware-configuration.nix ];
 
+      boot.kernelParams = [ "nvme_core.default_ps_max_latency_us=0" ];
+
       hardware.nvidia = {
         modesetting.enable = true;
         powerManagement.enable = true;
